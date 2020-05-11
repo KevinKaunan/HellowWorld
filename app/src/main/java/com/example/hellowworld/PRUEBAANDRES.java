7a39edc9aa18e3bbd854eb7ab0589ee7944cb0ed
AQUIL3S <andreshermosodemendoza@gmail.com>
2020-05-11 12:58:50 +0200

    MI PRUEBA

---
+++ app/src/main/java/com/example/hellowworld/PRUEBAANDRES.java
@@ -0,0 +1,14 @@
+package com.example.hellowworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PRUEBAANDRES extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_p_r_u_e_b_a_a_n_d_r_e_s);
+    }
+}
